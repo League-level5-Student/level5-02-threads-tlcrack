06a9db54924291051e777fed1e3386df6ba6031e
tlcrack <Tatecriqui@gmail.com>
2022-02-10 18:34:45 -0800

    OylmpicRings(ip)

---
+++ _01_Olympic_Rings/OlympicRings_Threaded.java
@@ -0,0 +1,11 @@
+package _01_Olympic_Rings;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class OlympicRings_Threaded {
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String[] args) {
+		Robot Terry = new Robot(40, 70);
+	}
+}
+
