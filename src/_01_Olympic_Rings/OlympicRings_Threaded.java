bf775ca464bb6a7a8fd197888f7530a3d293ad8c
League Student <league@iMac-20.attlocal.net>
2022-02-17 18:29:14 -0800

    OlympicRings, AdvancedRobotRace (ip)

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,75 @@
 package _01_Olympic_Rings;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	static Robot ari = new Robot(400, 400);
+	static Robot barry = new Robot(550, 475);
+	static Robot carrie = new Robot(700, 400);
+	static Robot dairy = new Robot(850, 475);
+	static Robot eerie = new Robot(1000, 400);
+	public static void main(String[] args) {
+		ari.setSpeed(10);
+		barry.setSpeed(10);
+		carrie.setSpeed(10);
+		dairy.setSpeed(10);
+		eerie.setSpeed(10);
 
+		ari.setPenColor(Color.blue);
+		barry.setPenColor(Color.yellow);
+		dairy.setPenColor(Color.green);
+		eerie.setPenColor(Color.red);
+		
+		ari.setPenWidth(10);
+		barry.setPenWidth(10);
+		carrie.setPenWidth(10);
+		dairy.setPenWidth(10);
+		eerie.setPenWidth(10);
+		
+		ari.penDown();
+		barry.penDown();
+		carrie.penDown();
+		dairy.penDown();
+		eerie.penDown();
+
+		ari.hide();
+		barry.hide();
+		carrie.hide();
+		dairy.hide();
+		eerie.hide();
+		
+		Thread air = new Thread(()->encircle(ari));
+		Thread bear = new Thread(()->encircle(barry));
+		Thread care = new Thread(()->encircle(carrie));
+		Thread dare = new Thread(()->encircle(dairy));
+		Thread ear = new Thread(()->encircle(eerie));
+		
+		air.start();
+		bear.start();
+		care.start();
+		dare.start();
+		ear.start();
+		try {
+			air.join();
+			bear.join();
+			care.join();
+			dare.join();
+			ear.join();
+		}
+		catch(Exception e) {
+			
+		}
+	}
+	
+	static void encircle(Robot fairy) {
+		for(int i = 0; i < 360; i++) {
+			fairy.move(2);
+			fairy.turn(1);
+		}
+		
+	}
 }
 
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,60 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
 	// Re-do the robot race recipe from level 3 module 0. 
 	// This time, use threads to make all of the robots go at the same time.
+	static Random r = new Random();
+	static int z = 0;
+	static int w = 0;
+	static Robot[] rob = new Robot[5];
+
+	static int winner = 0;
+	static boolean finished = false;
+	
+	public static void main(String[] args) {
+
+		for (int i = 0; i < rob.length; i++) {
+			rob[i] = new Robot("mini");
+			rob[i].setSpeed(10);
+			rob[i].penDown();
+		}
+		for (int s = 0; s < 5; s++) {
+			rob[s].setX(s * 20 + 200);
+			rob[s].setY(250);
+		}
+		
+
+		Thread racer = new Thread(()->race());
+		
+		while (finished == false) {
+			for (w = 0; w < 5; w++) {
+				racer.start();
+				try {
+					racer.join();
+				}
+				catch(Exception e) {
+					
+				}
+				if(finished==true) {
+					break;
+				}
+			}
+		}
+		System.out.println("robot#" + winner + " is the winner!");
+	}
+	static void race() {
+		for (z = r.nextInt(50); z > 0; z--) {
+			rob[w].move(1);
+			rob[w].turn(1);
+			if (rob[w].getX() == w * 20 + 200 && rob[w].getY() == 250) {
+				finished = true;
+				winner = w + 1;
+				break;
+			}
+		}
+	}
 }
